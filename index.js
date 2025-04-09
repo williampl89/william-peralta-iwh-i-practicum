d80167973e2535c2d0683fe4b996cb041fd2bb30
William Peralta <wilnesta@gmail.com>
2025-04-09 12:28:33 -0600

    Add style neccesary

--- index.js
+++ index.js
@@ -37,6 +37,14 @@ app.get('/', async (req, res) => {
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.get('/update-cobj', (req, res) => {
+    try {
+        res.render('update-cobj', { title: 'Create new Patient | HubSpot APIs' });      
+    } catch (error) {
+        console.error(error);
+    }
+
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
